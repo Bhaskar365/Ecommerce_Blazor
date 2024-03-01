1bafc9ccc0021823bf728cd57311255ff39f7d18
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-01 19:41:17 +0530

    adding classes for paypal payment method in logic section

--- Shop.API/obj/Debug/net6.0/Shop.API.AssemblyInfo.cs
+++ Shop.API/obj/Debug/net6.0/Shop.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b4fc36f05e67563173b413b8e9b93938d2d9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d7124bceed51a40bca88549ba271004831e5f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

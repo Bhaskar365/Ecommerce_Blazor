b479272c17306ffe35f8af6f6e6f274cbbb283c8
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-04 19:22:42 +0530

    paypal api call errors resolved

--- Shop.Logic/PayPal/PayPalAPI.cs
+++ Shop.Logic/PayPal/PayPalAPI.cs
@@ -18,6 +18,8 @@ namespace Shop.Logic.PayPal
         {
             configuration = _configuration;
         }
+
+        //body response for payment method checker
         public async Task<string> getRedirectURLToPayPal(double total, string currency)
         {
             try
